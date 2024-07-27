57981592abf414c8b4b867dc130825e711d6da75
CuDuyDuc <duccu1403@gmail.com>
2024-07-28 00:07:52 +0700

    push first

---
+++ src/assets/colors/Colors.ts
@@ -0,0 +1,12 @@
+const COLORS = {
+    GREEN: '#009245',
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+    HEX_LIGHT_GRAY : '#52555A',
+    HEX_LIGHT_GREY: '#AEAEAE',
+    RED : 'red',
+    HEX_LIGHT: '#141921',
+    LIGHT: '#d9e3f0'
+}
+
+export default COLORS;
---
+++ src/assets/images/Images.ts
@@ -0,0 +1,15 @@
+const IMAGES = {
+    ForgotPassWord: require('./forgotpass.png'),
+    Login: require('./login.png'),
+    Logo: require('./logo.png'),
+    Onboarding: require('./onboarding1.png'),
+    Onboarding1: require('./onboarding2.png'),
+    Onboarding2: require('./onboarding3.png'),
+    OTPEmail: require('./otp.png'),
+    SignUp: require('./signup.png'),
+    SlideShow: require('./slideshow1.png'),
+    SlideShow1: require('./slideshow2.png'),
+    SlideShow2: require('./slideshow3.png'),
+}
+
+export default IMAGES
