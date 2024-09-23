144901bd7477ab79f3c9c4b6807825640129eb15
CuDuyDuc <duccu1403@gmail.com>
2024-09-24 02:22:53 +0700

    Done UI SignUpScreen & ForgotPassWord

--- src/screens/auth/ForgotPassWord.tsx
+++ src/screens/auth/ForgotPassWord.tsx
@@ -1,11 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { Validate } from '../../utils/validate';
+import { globalStyle } from '../../styles/globalStyle';
+import { ButtonComponent, InputComponent, SectionComponent, TextComponent } from '../../components';
+import { ArrowLeft2, Sms } from 'iconsax-react-native';
+import COLORS from '../../assets/colors/Colors';
+import { FONTFAMILY } from '../../../assets/fonts';
+import { LoadingModal } from '../../modal';
+import IMAGES from '../../assets/images/Images';
 
-const ForgotPassWord = () => {
+const ForgotPassWord = ({ navigation }: any) => {
+    const [email, setEmail] = useState('');
+    const [isDisable, setIsDisable] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleCheckEmail = () => {
+        const isValidEmail = Validate.email(email);
+        setIsDisable(!isValidEmail);
+    }
 
     return (
-        <View>
-            <Text>ForgotPassWord</Text>
+        <View style={{ flex: 1, backgroundColor: COLORS.WHISPER_GRAY }}>
+            <SectionComponent>
+                <TouchableOpacity style={{ paddingTop: 50, paddingBottom: 10 }}>
+                    <ArrowLeft2 size={24} color={COLORS.OCEAN_BLUE}
+                        onPress={() => navigation.goBack()} />
+                </TouchableOpacity>
+                <TextComponent text='Resset Mật Khẩu' title font={FONTFAMILY.montserrat_bold} color={COLORS.OCEAN_BLUE} />
+                <TextComponent text='Để đặt lại mật khẩu, bạn cần có email có thể được xác thực.' styles={{ paddingBottom: 30 }} color={COLORS.BLUE_GRAY} />
+            </SectionComponent>
+            <SectionComponent styles={{ alignItems: 'center'}}>
+                <Image source={IMAGES.ForgotPass}/>
+            </SectionComponent>
+            <SectionComponent styles={{marginTop: 40}}>
+                <InputComponent
+                    value={email}
+                    placeholder='abc123@gmail.com'
+                    onChange={val => setEmail(val)}
+                    backgroundColor={COLORS.WHITE}
+                    affix={<Sms size={24} color={COLORS.HEX_LIGHT_GREY} />}
+                    onEnd={handleCheckEmail} />
+            </SectionComponent>
+            <SectionComponent styles={{ alignItems: 'center' }}>
+                <ButtonComponent
+                    text='Gửi'
+                    type='#00ADEF'
+                    styles={{ width: '80%' }}
+                    disable={isDisable} />
+            </SectionComponent>
+            <LoadingModal visible={isLoading} />
         </View>
     )
 }
--- src/screens/auth/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen.tsx
@@ -1,12 +1,171 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Validate } from '../../utils/validate';
+import { ButtonComponent, InputComponent, KeyboardAvoidingViewWrapper, RowComponent, SectionComponent, TextComponent } from '../../components';
+import IMAGES from '../../assets/images/Images';
+import { LoadingModal } from '../../modal';
+import COLORS from '../../assets/colors/Colors';
+import { Lock, Sms, User } from 'iconsax-react-native';
+import { FONTFAMILY } from '../../../assets/fonts';
 
-const SignUpScreen = () => {
+interface ErrorMessages {
+    email: string;
+    password: string;
+    confirmPass: string;
+}
+
+const initValues = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPass: '',
+}
+
+
+const SignUpScreen = ({ navigation }: any) => {
+    const [values, setValues] = useState(initValues);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState<any>();
+    const [isDisable, setIsDisable] = useState(true);
+
+    useEffect(() => {
+        if (
+            !errorMessage ||
+            (errorMessage &&
+                (errorMessage.email ||
+                    errorMessage.password ||
+                    errorMessage.confirmPass)) ||
+            !values.email ||
+            !values.password ||
+            !values.confirmPass
+        ) {
+            setIsDisable(true);
+        } else {
+            setIsDisable(false);
+        }
+    }, [errorMessage, values]);
+
+
+    const handleChangeValue = (key: string, value: string) => {
+        const data: any = { ...values }
+        data[`${key}`] = value;
+        setValues(data);
+    }
+
+    const formValidator = (key: string) => {
+        const data = { ...errorMessage };
+        let message = ``;
+
+        switch (key) {
+            case 'email':
+                if (!values.email) {
+                    message = `Vui lòng nhập Email!`;
+                } else if (!Validate.email(values.email)) {
+                    message = 'Email không hợp lệ!';
+                } else {
+                    message = '';
+                }
+
+                break;
+
+            case 'password':
+                message = !values.password ? `Vui lòng nhập Password` : '';
+                break;
+
+            case 'confirmPass':
+                if (!values.confirmPass) {
+                    message = `Vui lòng nhập xác nhận mật khẩu!`;
+                } else if (values.confirmPass !== values.password) {
+                    message = 'Mật khẩu không khớp!';
+                } else {
+                    message = '';
+                }
+
+                break;
+        }
+
+        data[`${key}`] = message;
+
+        setErrorMessage(data);
+    };
 
     return (
-        <View>
-            <Text>SignUpScreen</Text>
-        </View>
+        <>
+            <KeyboardAvoidingViewWrapper>
+                <SectionComponent styles={{ alignItems: 'center', marginTop: 70 }}>
+                    <Image source={IMAGES.SignUp} />
+                </SectionComponent>
+                <SectionComponent>
+                    <TextComponent
+                        title
+                        text='Đăng Ký'
+                        size={33}
+                        color={COLORS.OCEAN_BLUE}
+                        font={FONTFAMILY.montserrat_bold}
+                        styles={{ marginBottom: 50, textAlign:'center'}} />
+                    <InputComponent
+                        value={values.username}
+                        placeholder='Họ tên'
+                        onChange={val => handleChangeValue('username', val)}
+                        allowClear
+                        backgroundColor={COLORS.WHITE}
+                        affix={<User size={22} color={COLORS.BLUE_GRAY} />} />
+                    <InputComponent
+                        value={values.email}
+                        placeholder='Email'
+                        onChange={val => handleChangeValue('email', val)}
+                        allowClear
+                        backgroundColor={COLORS.WHITE}
+                        affix={<Sms size={22} color={COLORS.BLUE_GRAY} />}
+                        onEnd={() => formValidator('email')} />
+                    <InputComponent
+                        value={values.password}
+                        placeholder='Mật khẩu'
+                        onChange={val => handleChangeValue('password', val)}
+                        isPassword
+                        backgroundColor={COLORS.WHITE}
+                        affix={<Lock size={22} color={COLORS.BLUE_GRAY} />}
+                        onEnd={() => formValidator('password')} />
+                    <InputComponent
+                        value={values.confirmPass}
+                        placeholder='Xác nhận mật khẩu'
+                        onChange={val => handleChangeValue('confirmPass', val)}
+                        isPassword
+                        backgroundColor={COLORS.WHITE}
+                        affix={<Lock size={22} color={COLORS.BLUE_GRAY} />}
+                        onEnd={() => formValidator('confirmPass')} />
+                </SectionComponent>
+                {errorMessage && (
+                    <SectionComponent>
+                        {Object.keys(errorMessage).map(
+                            (error, index) =>
+                                errorMessage[`${error}`] && (
+                                    <TextComponent
+                                        text={errorMessage[`${error}`]}
+                                        key={`error${index}`}
+                                        color={COLORS.RED}
+                                    />
+                                ),
+                        )}
+                    </SectionComponent>
+                )}
+                <SectionComponent styles={{ marginTop: 20 }}>
+                    <ButtonComponent
+                        text='ĐĂNG KÝ'
+                        type='#00ADEF'
+                        disable={isDisable} />
+                </SectionComponent>
+                <SectionComponent styles={{ marginTop: 20 }}>
+                    <RowComponent justify='center'>
+                        <TextComponent text="Bạn đã có tài khoản?  "  color={COLORS.BLUE_GRAY}/>
+                        <ButtonComponent type='link' text='Đăng nhập' onPress={() => {
+                            navigation.navigate('LoginScreen')
+                        }} />
+                    </RowComponent>
+                </SectionComponent>
+            </KeyboardAvoidingViewWrapper >
+            <LoadingModal visible={isLoading} />
+        </>
     )
 }
 
