98ec5c14372f216b0c429179be7db127514e5e0e
CuDuyDuc <duccu1403@gmail.com>
2024-10-14 14:07:09 +0700

    New Update

--- src/routers/role_router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const { addRole, getRole } = require('../controllers/role_controller')
-
-const RoleRouter = Router()
-
-RoleRouter.post('/add-role',addRole)
-RoleRouter.post('/get-role',getRole)
-
-module.exports = RoleRouter
