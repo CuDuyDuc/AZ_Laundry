654075f94c287274c0a055dc69679dc1feabd053
CuDuyDuc <duccu1403@gmail.com>
2024-10-17 13:56:52 +0700

    new FE

--- src/components/CardServiceComponent.tsx
+++ src/components/CardServiceComponent.tsx
@@ -3,21 +3,17 @@ import { View, Text, Image, FlatList, StyleSheet, ActivityIndicator, TouchableOp
 import COLORS from '../assets/colors/Colors';
 import serviceAPI from '../apis/serviceAPI';
 import { FONTFAMILY } from '../../assets/fonts';
+import { service_type } from '../model/service_type';
 
-interface ServiceType {
-    _id: string;
-    service_type_name: string;
-    service_type_icon: string;
-}
 
 const CardServiceComponent = () => {
-    const [typeService, setTypeService] = useState<ServiceType[]>([]);
+    const [typeService, setTypeService] = useState<service_type[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     const getDataService_Type = async () => {
         try {
             const res = await serviceAPI.HandleService('/get-service-type');
-            const data: ServiceType[] = await res.data;
+            const data: service_type[] = await res.data;
             setTypeService(data);
             setLoading(false);
         } catch (error) {
@@ -30,7 +26,7 @@ const CardServiceComponent = () => {
         getDataService_Type();
     }, []);
 
-    const renderItem = ({ item }: { item: ServiceType }) => (
+    const renderItem = ({ item }: { item: service_type }) => (
         <TouchableOpacity style={styles.card}>
             <Image source={{ uri: item.service_type_icon }} style={styles.icon} />
             <Text style={styles.serviceName}>{item.service_type_name}</Text>
@@ -46,7 +42,7 @@ const CardServiceComponent = () => {
                     <FlatList
                         data={typeService}
                         renderItem={renderItem}
-                        keyExtractor={(item) => item._id}
+                        keyExtractor={(item) => item._id.toString()}
                         numColumns={3}
                         columnWrapperStyle={styles.row}
                     />
