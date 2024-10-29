47d3d0e59cf95a40403d340e163f94edb4f81466
phuoccntt2004 <phuoccntt2004@gmail.com>
2024-10-29 17:07:28 +0700

    done fix cart

--- src/components/CardProductOfProductTypeComponent.tsx
+++ src/components/CardProductOfProductTypeComponent.tsx
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, Image, ActivityIndicator, FlatList } from 'react-native';
+import { View, TouchableOpacity, Image, ActivityIndicator, FlatList, Animated } from 'react-native';
 import React from 'react';
 import { ProductModel } from '../model/product';
 import { CartModel } from '../model/cart_model';
@@ -11,6 +11,8 @@ import { useAxiosAddCart } from '../hooks/useAxiosAddCart';
 import { useSelector } from 'react-redux';
 import { authSelector } from '../redux/reducers/authReducer';
 import { useRole } from '../permission/permission';
+import SectionComponent from './SectionComponent';
+import { Swipeable } from 'react-native-gesture-handler';
 
 interface Props {
     products?: ProductModel[];
@@ -19,34 +21,54 @@ interface Props {
     isCart?: boolean;
     onPressMinus?: (data: CartModel) => void ;
     onPressPlus?: (data: CartModel) => void ;
+    onPressDelete?: (data: CartModel) => void ;
 }
 
 const CardProductOfProductType = (props: Props) => {
     const user = useSelector(authSelector);
     const { isUser } = useRole();
-    const { products, carts, isLoading, isCart,onPressMinus, onPressPlus } = props;
+    const { products, carts, isLoading, isCart,onPressMinus, onPressPlus ,onPressDelete} = props;
+    const renderRightActions = (item:CartModel) => (
+        (progressAnimatedValue: Animated.AnimatedInterpolation<string | number>, dragAnimatedValue: Animated.AnimatedInterpolation<string | number>) => (
+            <TouchableOpacity onPress={() => onPressDelete && onPressDelete(item)}>
+                <SectionComponent
+                    styles={{
+                        backgroundColor: COLORS.RED,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 75,
+                        height: '85%',
+                        borderRadius: 10,
+                    }}>
+                    <TextComponent text={'Xoá'}  font={FONTFAMILY.montserrat_medium}/>
+                </SectionComponent>
+            </TouchableOpacity>
+        )
+    );
     const renderCartItem = ({ item }: { item: CartModel }) => (
-        <TouchableOpacity style={{ marginBottom: 15, backgroundColor: COLORS.WHITE, padding: 8, borderRadius: 16 }}>
-            <RowComponent>
-                <Image source={{ uri: item.id_product.product_photo[0] }} style={{ width: 80, height: 80, borderRadius: 8 }} />
-                <View style={{ width: '78%', paddingHorizontal: 10 }}>
-                    <TextComponent text={item.id_product.product_name} color={COLORS.DARK_BLUE} font={FONTFAMILY.montserrat_bold} size={13} />
-                    <TextComponent styles={{ paddingVertical: 3 }} text={item.id_product.short_description} color={COLORS.HEX_LIGHT_GRAY} size={13} />
-                    <RowComponent justify='space-between'>
-                        <TextComponent text={`${item.cart_subtotal} vnđ`} color={COLORS.HEX_BLACK} font={FONTFAMILY.montserrat_medium} />
+        <Swipeable renderRightActions={renderRightActions(item)}>
+            <TouchableOpacity style={{ marginBottom: 15, backgroundColor: COLORS.WHITE, padding: 8, borderRadius: 16 }}>
+                <RowComponent>
+                    <Image source={{ uri: item.id_product.product_photo[0] }} style={{ width: 80, height: 80, borderRadius: 8 }} />
+                    <View style={{ width: '78%', paddingHorizontal: 10 }}>
+                        <TextComponent text={item.id_product.product_name} color={COLORS.DARK_BLUE} font={FONTFAMILY.montserrat_bold} size={13} />
+                        <TextComponent styles={{ paddingVertical: 3 }} text={item.id_product.short_description} color={COLORS.HEX_LIGHT_GRAY} size={13} />
                         <RowComponent justify='space-between'>
-                            <TouchableOpacity onPress={()=>onPressMinus && onPressMinus(item)}>
-                                <Minus size={25} variant='Bold' color={COLORS.AZURE_BLUE} />
-                            </TouchableOpacity>
-                            <TextComponent text={item.product_quantity.toString()} color={COLORS.GRAY_WHITE} font={FONTFAMILY.montserrat_medium} styles={{ marginHorizontal: 5 }} />
-                            <TouchableOpacity onPress={()=>onPressPlus && onPressPlus(item)}>
-                                <AddSquare size={25} variant='Bold' color={COLORS.AZURE_BLUE} />
-                            </TouchableOpacity>
+                            <TextComponent text={`${item.cart_subtotal} vnđ`} color={COLORS.HEX_BLACK} font={FONTFAMILY.montserrat_medium} />
+                            <RowComponent justify='space-between'>
+                                <TouchableOpacity onPress={()=>onPressMinus && onPressMinus(item)}>
+                                    <Minus size={25} variant='Bold' color={COLORS.AZURE_BLUE} />
+                                </TouchableOpacity>
+                                <TextComponent text={item.product_quantity.toString()} color={COLORS.GRAY_WHITE} font={FONTFAMILY.montserrat_medium} styles={{ marginHorizontal: 5 }} />
+                                <TouchableOpacity onPress={()=>onPressPlus && onPressPlus(item)}>
+                                    <AddSquare size={25} variant='Bold' color={COLORS.AZURE_BLUE} />
+                                </TouchableOpacity>
+                            </RowComponent>
                         </RowComponent>
-                    </RowComponent>
-                </View>
-            </RowComponent>
-        </TouchableOpacity>
+                    </View>
+                </RowComponent>
+            </TouchableOpacity>
+        </Swipeable>
     );
     const renderProductItem = ({ item }: { item: ProductModel }) => (
         <TouchableOpacity style={{ marginBottom: 15, backgroundColor: COLORS.WHITE, padding: 8, borderRadius: 16 }}>
