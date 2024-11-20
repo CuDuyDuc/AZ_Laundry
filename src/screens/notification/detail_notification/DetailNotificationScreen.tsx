0fdb134676908a73001b2139a2892e2220c5ee63
nghimartin <soobinnghi@gmail.com>
2024-11-20 20:37:20 +0700

    fix detail noti

--- src/screens/notification/detail_notification/DetailNotificationScreen.tsx
+++ src/screens/notification/detail_notification/DetailNotificationScreen.tsx
@@ -1,15 +1,40 @@
-import React, { useRef } from "react";
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View, Animated, Image } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View, Animated, Image, FlatList, ActivityIndicator } from "react-native";
 import COLORS from "../../../assets/colors/Colors";
 import { CardOrderComponent, HeaderComponent, KeyboardAvoidingViewWrapper, SectionComponent, TextComponent } from "../../../components";
 import { Box, DiscountShape, NotificationBing, ShoppingBag, TicketDiscount } from "iconsax-react-native";
 import { notification_type } from "../../../utils/constants";
+import paymentAPI from "../../../apis/paymentAPI";
 
 export default function DetailNotificationScreen({navigation, route}: any) {
     const { item } = route.params;
+    console.log(item);
+    const [dataPayment, setDataPayment] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(true);
+    console.log({dataPayment});
+
+    const getPaymentById = async (idPayment: string) => {
+        try {
+            const req = paymentAPI.HandlePayment(`/get-order-by-id/${idPayment}`);
+            setDataPayment((await req).data);
+               setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    const formatId = (text: string, maxLength: number = 10): string => {
+        if (!text || text.length <= maxLength) return text; 
+        return `${text.slice(-6)}`;
+      };
+      
+    useEffect(() => {
+      getPaymentById(item?.object_type_id);
+    }, [])
     
+    // Render nội dung dựa trên loại thông báo
     const renderNotificationContent = () => {
-        switch ("order_update") {
+        switch (item?.notification_type) {
             case notification_type.ORDER_UPDATE:
                 return (
                     <SectionComponent styles={styles.contentBox}>
@@ -18,35 +43,41 @@ export default function DetailNotificationScreen({navigation, route}: any) {
                             textAlign: 'center',
                             width: '100%'
                         }}  title text="Đơn Hàng Mới" />
-                        <TextComponent  styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Mã Đơn: #1981239188`} />
-                        <TextComponent  styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Khách Hàng: #1981239188`} />
                         <SectionComponent styles={{ display: 'flex', flexDirection: 'row', paddingHorizontal:0, paddingBottom: 0}}>
-                        <TextComponent  styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Phương thức thanh toán: `} />
-                        <TextComponent color={COLORS.AZURE_BLUE} size={16} text={`VNPAY`} />
+                        <TextComponent styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Mã Đơn: `} />
+                        <TextComponent color={COLORS.HEX_BLACK} size={16} text={`#${formatId(dataPayment?._id, 7)}`} />
                         </SectionComponent>
-                    
+                        <SectionComponent styles={{ display: 'flex', flexDirection: 'row', paddingHorizontal:0, paddingBottom: 0}}>
+                        <TextComponent styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Khách Hàng: `} />
+                        <TextComponent color={COLORS.AZURE_BLUE} size={16} text={dataPayment?.id_user?.fullname || 'fullname'} />
+                        </SectionComponent>
+                       
+                        <SectionComponent styles={{ display: 'flex', flexDirection: 'row', paddingHorizontal:0, paddingBottom: 0}}>
+                        <TextComponent styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Phương thức thanh toán: `} />
+                        <TextComponent color={COLORS.AZURE_BLUE} size={16} text={dataPayment?.method_payment || ''} />
+                        </SectionComponent>
+                        <SectionComponent styles={{ display: 'flex', flexDirection: 'row', paddingHorizontal:0, paddingBottom: 0}}>
+                        <TextComponent styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Tổng Tiền: `} />
+                        <TextComponent color={COLORS.AZURE_BLUE} size={16} text={`${dataPayment?.mount_money} VND`} />
+                        </SectionComponent>
 
-                        <TextComponent  styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Tổng Tiền: 120.000 đ`} />
-
-                        <TextComponent  styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Dịch vụ khách đặt:`} />
-                        <CardOrderComponent 
-                          imgUrl={"https://firebasestorage.googleapis.com/v0/b/az-laundry.appspot.com/o/product_type_icon%2Fao.png?alt=media&token=1aff5676-861a-42c1-b0c9-15ad3343e330"}
-                          name={"Áo"}
-                          short_description={"ok"}
-                          status={"Chờ duyệt"}
-                          total={140.000}
-                          quantity={2}
+                        <TextComponent styles={{ fontWeight: 'bold'}}  color={COLORS.HEX_BLACK} size={16} text={`Dịch vụ khách đặt:`} />
+                        <FlatList
+                        data={dataPayment?.id_cart || []}
+                        keyExtractor={(item: any) => item?._id.toString()}
+                        renderItem={({ item  }) => (
+                              <CardOrderComponent 
+                          imgUrl={item?.id_product?.product_photo[0] || ''}
+                          name={item?.id_product?.product_name || ''}
+                          short_description={item?.id_product?.short_description || ""}
+                          status={""}
+                          total={item?.cart_subtotal || 0}
+                          quantity={item?.product_quantity  || 0}
                           id={""}
                         />
-                        <CardOrderComponent 
-                          imgUrl={"https://firebasestorage.googleapis.com/v0/b/az-laundry.appspot.com/o/product_type_icon%2Fao.png?alt=media&token=1aff5676-861a-42c1-b0c9-15ad3343e330"}
-                          name={"Áo"}
-                          short_description={"ok"}
-                          status={"Chờ duyệt"}
-                          total={140.000}
-                          quantity={2}
-                          id={""}
+                        )}
                         />
+                     
                     </SectionComponent>
                 );
             case notification_type.REMINDER:
@@ -81,12 +112,11 @@ export default function DetailNotificationScreen({navigation, route}: any) {
         <SectionComponent styles={styles.container}>
             <HeaderComponent isBack onBack={() => navigation.goBack()} title="Thông báo chi tiết" />
         
-            <KeyboardAvoidingViewWrapper>
                 <SectionComponent styles={styles.scrollViewContent} >
                     <SectionComponent styles= {{
-                        marginTop: 40,
+                        margin: 20,
                         paddingHorizontal: 0,
-                        paddingVertical: 0,
+                        paddingBottom: 0,
                         backgroundColor: COLORS.WHITE,
                         width: 100,
                         height: 100,
@@ -99,9 +129,8 @@ export default function DetailNotificationScreen({navigation, route}: any) {
                         item?.notification_type === notification_type.PROMOTION ?  <DiscountShape size="40" color={COLORS.AZURE_BLUE}/> : 
                         item?.notification_type === notification_type.NEW_PRODUCT ? <Box size="40" color={COLORS.AZURE_BLUE}/> : <NotificationBing size="40" color={COLORS.AZURE_BLUE}/>}
                     </SectionComponent>
-                    {renderNotificationContent()}
+                    { loading ?  (<ActivityIndicator size="large" color={COLORS.OCEAN_BLUE}/>) :renderNotificationContent()}
                 </SectionComponent>
-            </KeyboardAvoidingViewWrapper>
         </SectionComponent>
     );
 }
