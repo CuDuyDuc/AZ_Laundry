0e1a71822e9025c6fe32b7c3d82d4889cd415485
cu thao <lexuanthao.tk7@gmail.com>
2024-12-03 00:54:16 +0700

    update OrderStatisticsScreen

--- src/components/CardOrderShopComponent.tsx
+++ src/components/CardOrderShopComponent.tsx
@@ -4,12 +4,14 @@ import SectionComponent from "./SectionComponent";
 import { RowComponent, TextComponent } from "./index";
 import COLORS from "../assets/colors/Colors";
 import { FONTFAMILY } from "../../assets/fonts";
+import { useSelector } from "react-redux";
+import { authSelector } from "../redux/reducers/authReducer";
 
 interface Props {
     id: string;
     status: string;
     price: number;
-    imgUrl?: string;
+    imgUrl?: any;
     dateOrder: string;
     onPress: () => void;
 }
@@ -23,14 +25,24 @@ const CardOrderShopComponent = (props: Props) => {
         imgUrl , // Default value
         onPress,
     } = props;
-    
+    const productPhotos: any = []  
+    const user = useSelector(authSelector)
+    imgUrl.forEach((cartItem:any) => {
+        // Kiểm tra điều kiện id_user trong id_product khớp với userId
+        if (cartItem.id_product.id_user.toString() === user.id.toString()) {
+          // Lấy product_photo[0] và thêm vào mảng
+          if (cartItem.id_product.product_photo?.[0]) {
+            productPhotos.push(cartItem.id_product.product_photo[0]);
+          }
+        }
+      });
     return (
         <TouchableOpacity
             onPress={onPress}
             style={{ marginBottom: 15, backgroundColor: COLORS.WHITE, padding: 8, borderRadius: 16, elevation: 5 }}
         >
             <RowComponent>
-                <Image source={{ uri: imgUrl }} style={{ width: 80, height: 80, marginRight: 10, borderRadius:8 }} />
+                <Image source={{ uri: productPhotos[0] }} style={{ width: 80, height: 80, marginRight: 10, borderRadius:8 }} />
                 <View style={{ width: '78%' }}>
                     <RowComponent justify="space-between">
                         <TextComponent text={`Mã đơn: ${id}`} color={COLORS.BLUE_GRAY} size={15} font={FONTFAMILY.montserrat_bold} />
